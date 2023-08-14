63fdc4d3bb6c38822c0d261ad97c009b0a7980c1
peter <apppeterpan@gmail.com>
2023-08-14 17:36:57 +0800

    Initial Commit

---
+++ DemoEmoji/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoEmoji
+//
+//  Created by Peter Pan on 2023/8/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoEmoji/DemoEmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoEmojiApp.swift
+//  DemoEmoji
+//
+//  Created by Peter Pan on 2023/8/14.
+//
+
+import SwiftUI
+
+@main
+struct DemoEmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
